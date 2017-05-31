a943adb238fe95c84b81de30fbab691ac42e76e2
Kevin Johnson <kevinjohnsonpublic@gmail.com>
2017-05-30 23:34:46 -0400

    fix(zipCode): correctly handle zip codes and padding (#21)

---
+++ src/filters/zip-code.filter.js
@@ -0,0 +1,27 @@
+'use strict';
+'ngInject';
+
+angular.module('appliancePointOfSale').filter('zipCode', function() {
+  return function (code) {
+    if (!code) { return ''; }
+
+    const value = code.replace(/[^0-9]/g, '');
+
+    // uncomment this when zip codes are strings
+    // switch (value.length) {
+    // case 0:
+    // case 1:
+    // case 2:
+    // case 3:
+    // case 4:
+    // case 5:
+    //   return value;
+    // default:
+    //   const zipCode = value.slice(0, 5);
+    //   const plusFour = value.slice(5, 9);
+    //   return zipCode + '-' + plusFour;
+    // }
+
+    return value.slice(0, 5);
+  };
+});
---
+++ src/ticket-panel/zip-code-input.directive.js
@@ -0,0 +1,47 @@
+'use strict';
+'ngInject';
+
+angular.module('appliancePointOfSale').directive('zipCodeInput', function($filter, $browser) {
+  return {
+    require: 'ngModel',
+    link: function($scope, $element, $attrs, ngModelCtrl) {
+      const listener = () => {
+        $element.val($filter('zipCode')($element.val()));
+      };
+
+      // This runs when we update the text field
+      ngModelCtrl.$parsers.push((viewValue) => {
+        const value = viewValue.replace(/[^0-9]/g, '');
+        let zipCode = '00000';
+        if (value.length <= 5) {
+          zipCode = zipCode.slice(0, 5 - value.length) + value;
+        } else {
+          zipCode = value.slice(0, 5);
+          // uncomment this when zip codes are strings
+          // zipCode = value.slice(0, 5) + '-' + value.slice(5, 9);
+        }
+        return zipCode;
+      });
+
+      // This runs when the model gets updated on the scope directly and keeps our view in sync
+      ngModelCtrl.$render = () => {
+        $element.val($filter('zipCode')(ngModelCtrl.$viewValue));
+      };
+
+      $element.bind('change', listener);
+      $element.bind('keydown', (event) => {
+        var key = event.keyCode;
+        // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+        // This lets us support copy and paste too
+        if (key === 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
+          return;
+        }
+        $browser.defer(listener); // Have to do this or changes don't get picked up properly
+      });
+
+      $element.bind('paste cut', function() {
+        $browser.defer(listener);
+      });
+    }
+  };
+});
